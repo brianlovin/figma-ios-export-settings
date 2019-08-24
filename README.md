33c67cc46d39922ccc5ed40227b87f0c48d289a8
Brian Lovin <briandlovin@gmail.com>
2019-08-24 15:13:22 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+  ![Figma iOS Export Settings](./assets/cover.png)
+
+# Figma iOS Export Settings 📱
+
+Instantly apply the required export settings for iOS app icons.
+
+[Install on Figma]()
+
+## Usage
+
+1. Select a frame containing your app icon.
+2. Press `command + /` and search for 'iOS Export Settings'
+3. Run the plugin
+4. `command + shift + p` to re-run the plugin instantly
+ 
+## Installation and contributing
+
+1. Clone the repository: `git clone https://github.com/brianlovin/figma-ios-export-settings.git`
+1. Go to the directory: `cd figma-ios-export-settings`
+1. Install `figplug`: `npm i -g figplug`
+1. Build the plugin: `figplug build -w -o=build`
+1. Add a new development plugin to Figma
+1. Select the `figma-ios-export-settings/build/manifest.json` file as the manifest
---
+++ src/plugin.ts
@@ -0,0 +1,35 @@
+const { selection } = figma.currentPage
+
+function hasValidSelection(nodes) {
+  return !(!nodes || nodes.length === 0)
+}
+
+const settings = [
+  { format: "PNG", suffix: "-20@1x", constraint: { type: "WIDTH", value: 20 }},
+  { format: "PNG", suffix: "-20@2x", constraint: { type: "WIDTH", value: 40 }},
+  { format: "PNG", suffix: "-20@3x", constraint: { type: "WIDTH", value: 60 }},
+  { format: "PNG", suffix: "-29@1x", constraint: { type: "WIDTH", value: 29 }},
+  { format: "PNG", suffix: "-29@2x", constraint: { type: "WIDTH", value: 58 }},
+  { format: "PNG", suffix: "-20@3x", constraint: { type: "WIDTH", value: 87 }},
+  { format: "PNG", suffix: "-40@1x", constraint: { type: "WIDTH", value: 40 }},
+  { format: "PNG", suffix: "-40@2x", constraint: { type: "WIDTH", value: 80 }},
+  { format: "PNG", suffix: "-40@3x", constraint: { type: "WIDTH", value: 120 }},
+  { format: "PNG", suffix: "-60@2x", constraint: { type: "WIDTH", value: 120 }},
+  { format: "PNG", suffix: "-60@3x", constraint: { type: "WIDTH", value: 180 }},
+  { format: "PNG", suffix: "-76@1x", constraint: { type: "WIDTH", value: 76 }},
+  { format: "PNG", suffix: "-76@2x", constraint: { type: "WIDTH", value: 152 }},
+  { format: "PNG", suffix: "-83.5@2x", constraint: { type: "WIDTH", value: 167 }},
+  { format: "PNG", suffix: "-1024@1x", constraint: { type: "WIDTH", value: 1024 }},
+]
+
+async function main(nodes): Promise<string> {
+  if (!hasValidSelection(nodes)) return Promise.resolve('No valid selection')
+
+  for (let node of nodes) {
+    node.exportSettings = settings
+  }
+
+  return Promise.resolve('Done!')
+}
+
+main(selection).then((res) => figma.closePlugin(res))
